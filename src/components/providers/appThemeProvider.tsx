6357c81f60b7fe45a7379528c0b631754ea770b2
Luiz Felipe <luizfelipe@Luizs-MacBook-Pro.local>
2025-05-17 11:19:20 -0400

    fix build

--- src/components/providers/appThemeProvider.tsx
+++ src/components/providers/appThemeProvider.tsx
@@ -3,6 +3,6 @@
 import { ThemeProvider as MTWProvider } from "@material-tailwind/react";
 
 export function AppThemeProvider({ children }: { children: React.ReactNode }) {
-  // return <MTWProvider>{children}</MTWProvider>;
-  return <>{children}</>; // TODO: Fast fix... Unsure of the impacts of not using ThemeProvider
+  return <MTWProvider>{children}</MTWProvider>;
+  // return <>{children}</>; // TODO: Fast fix... Unsure of the impacts of not using ThemeProvider
 }
