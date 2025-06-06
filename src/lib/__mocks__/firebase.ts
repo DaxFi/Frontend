9950e292b3c96c02da3ed3021596b3bf358b76ac
Clive <cad324@cornell.edu>
2025-06-05 21:22:44 -0400

    initial test config

---
+++ src/lib/__mocks__/firebase.ts
@@ -0,0 +1,5 @@
+export const app = {}; // dummy app object
+export const db = {}; // dummy db object
+export const collection = jest.fn();
+export const addDoc = jest.fn();
+export const serverTimestamp = jest.fn(() => "MOCK_TIMESTAMP");
